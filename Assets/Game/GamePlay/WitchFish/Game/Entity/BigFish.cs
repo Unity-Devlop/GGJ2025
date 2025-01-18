3981b835fedef6c4c609a56d62e337e273f4dc16
周圆 <476508162@qq.com>
2025-01-18 17:49:33 +0800

    feat: 大鱼讲话

--- Assets/Game/GamePlay/WitchFish/Game/Entity/BigFish.cs
+++ Assets/Game/GamePlay/WitchFish/Game/Entity/BigFish.cs
@@ -1,17 +1,28 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using Random = System.Random;
 
 namespace WitchFish
 {
     public class BigFish : MonoBehaviour
     {
         [SerializeField] private TMP_Text _text;
-
+        private int IndexChat = 0;
+        private List<string> list = new List<string>()
+        {
+            "aaaaaaaaa",
+            "bbbbbbbb",
+            "cccc",
+            "ddd",
+        };
         private void Awake()
         {
             GameMgr.Singleton.lakeFishCount.Register(OnLakeFishCountChange);
+            StartCoroutine(SpeakInterval());
         }
 
         private async void OnLakeFishCountChange(int obj)
@@ -26,6 +37,21 @@ namespace WitchFish
             if (GameMgr.SingletonNullable != null)
             {
                 GameMgr.Singleton.lakeFishCount.UnRegister(OnLakeFishCountChange);
+            }
+        }
+
+        IEnumerator SpeakInterval()
+        {
+            while (true) // 无限循环
+            {
+
+                var time = UnityEngine.Random.Range(6,8);
+                _text.text = list[IndexChat%list.Count];
+                IndexChat++;
+                yield return new WaitForSeconds(1);
+                _text.text = string.Empty;
+                // 等待指定的秒数
+                yield return new WaitForSeconds(time);
             }
         }
     }
