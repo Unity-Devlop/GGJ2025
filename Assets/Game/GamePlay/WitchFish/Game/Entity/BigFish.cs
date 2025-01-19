423b0905f62c89f830c80e717b7558be1a325a5b
NicoIer <NicoIer@163.com>
2025-01-19 13:14:49 +0800

    大鱼说话

--- Assets/Game/GamePlay/WitchFish/Game/Entity/BigFish.cs
+++ Assets/Game/GamePlay/WitchFish/Game/Entity/BigFish.cs
@@ -14,6 +14,7 @@ namespace WitchFish
         public GameObject panel;
         [SerializeField] private TMP_Text _text;
         private int IndexChat = 0;
+
         private List<string> whenJumpInLake = new List<string>()
         {
             "真是太逊了，你的鱼塘里才{0}只鱼。",
@@ -21,26 +22,21 @@ namespace WitchFish
             "你才养了{0}只鱼，我奶奶玩得都比你好。",
             "{0}条鱼，你到底行不行啊牢弟？",
             "现在才{0}条鱼，把鼠标放下，让那块肥皂玩。",
-
         };
 
         private List<string> whenInTime = new List<string>()
         {
-
             "把核弹捡起来，把核弹捡起来！",
             "你要不先去玩会三消练练手速吧？",
             "收手吧牢弟，手机里全都是咸鱼。",
             "就这？",
             "终于有点鱼了，烧火准备吃火锅吧！",
-
         };
 
         private List<string> whenDie = new List<string>()
         {
-
             "又有一条鱼在你的鱼塘里永远沉眠了。",
             "你的鱼淹死了。什么？你说是饿死的？那你不赶紧喂鱼！",
-
         };
 
         private List<string> whenRun = new List<string>()
@@ -48,14 +44,19 @@ namespace WitchFish
             "不配合的鱼抓回来电疗！",
             "你吃菌子中毒了吧？哪有鱼走？",
         };
+
+        Queue<string> queue = new Queue<string>();
+
         private void Awake()
         {
             GameMgr.Singleton.lakeFishCount.Register(OnLakeFishCountChange);
-            StartCoroutine(SpeakInterval());
+            StartCoroutine(AutoEnqueue());
             Core.Event.Listen<EventFishDieInLandPush>(OnSendFishDieInLand);
             Core.Event.Listen<EventFishDieInLakePush>(OnSendFishDieInLakePush);
             Core.Event.Listen<EventFishJumpInLakePush>(OnSendFishJumpInLakePush);
+            ExecuteQueue().Forget();
         }
+
 
         private async void OnLakeFishCountChange(int obj)
         {
@@ -80,49 +81,61 @@ namespace WitchFish
         {
             Global.cameraSystem.SetToMenuCamera();
             panel.gameObject.SetActive(true);
-
         }
 
-        IEnumerator SpeakInterval()
+
+        private async UniTask ExecuteQueue()
+        {
+            while (!destroyCancellationToken.IsCancellationRequested)
+            {
+                if (queue.Count == 0)
+                {
+                    await UniTask.Yield();
+                    continue;
+                }
+                var str = queue.Dequeue();
+                _text.text = str;
+                await UniTask.Delay(TimeSpan.FromSeconds(0.1 * str.Length));
+                _text.text = "";
+            }
+        }
+
+        IEnumerator AutoEnqueue()
         {
             while (true) // 无限循环
             {
-
-                var time = UnityEngine.Random.Range(15,20);
+                var time = UnityEngine.Random.Range(15, 20);
                 yield return new WaitForSeconds(time);
-                _text.text = whenInTime[IndexChat% whenInTime.Count];
+                queue.Enqueue(whenInTime[IndexChat % whenInTime.Count]);
                 IndexChat++;
-                yield return new WaitForSeconds(3);
-                _text.text = string.Empty;
-                // 等待指定的秒数
             }
         }
 
 
         void OnSendFishDieInLand(EventFishDieInLandPush push)
         {
-            StartCoroutine( SpeakByPush(whenRun,push.pa));
+            queue.Enqueue(GetSpeakStr(whenRun, push.pa));
         }
+
         void OnSendFishDieInLakePush(EventFishDieInLakePush push)
         {
-            StartCoroutine(SpeakByPush(whenDie,push.pa));
+            queue.Enqueue(GetSpeakStr(whenDie, push.pa));
         }
+
         void OnSendFishJumpInLakePush(EventFishJumpInLakePush push)
         {
             // Debug.LogError(push.pa);
-            if(int.Parse( push.pa)%10 == 0)
+            if (int.Parse(push.pa) % 10 == 0)
             {
-                StartCoroutine(SpeakByPush(whenJumpInLake,push.pa));
+                queue.Enqueue(GetSpeakStr(whenJumpInLake, push.pa));
             }
         }
 
 
-        IEnumerator SpeakByPush(List<string> list,string pa)
+        string GetSpeakStr(List<string> list, string pa)
         {
             var index = UnityEngine.Random.Range(0, list.Count);
-            _text.text = string.Format( list[index],pa);
-            yield return new WaitForSeconds(3);
-            _text.text = string.Empty;
+            return string.Format(list[index], pa);
         }
     }
 }
