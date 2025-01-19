8ade4c8ff09e7e93deb336de709c76fe30d82507
NicoIer <NicoIer@163.com>
2025-01-19 11:30:22 +0800

    1

--- Assets/Game/GamePlay/WitchFish/Game/Soap.cs
+++ Assets/Game/GamePlay/WitchFish/Game/Soap.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Game;
 using UnityEngine;
+using UnityEngine.Serialization;
 using WitchFish;
 using Random = UnityEngine.Random;
 
@@ -10,95 +11,156 @@ namespace WitchFish
 {
     public class Soap : MonoBehaviour
     {
-        public  bool isCleaningFish = false;
-        public Vector3 initPos = new Vector3(0,0,0);
-        public Collider2D TargetFish = null;
-        public string colliderName = string.Empty;
+        // public bool isCleaningFish = false;
+        // 上一次检测到鱼的位置
+        private Vector3 _prevHitFishPos = new Vector3(0, 0, 0);
+
+        [Sirenix.OdinInspector.ShowInInspector, Sirenix.OdinInspector.ReadOnly]
+        private Collider2D _prevTarget = null;
+
+        public const string ColliderName = "SoapCollider";
         int cuoCount = 0;
 
         public Vector3 maxInPath;
         public Vector3 minInPath;
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame  
-        void Update()
-        {
-
-        }
 
         public int GetRandomExcluding(int min, int max, List<ItemEnum> exclude)
         {
             int randomValue = min;
-            while (exclude.Contains( (ItemEnum)randomValue) )
+            while (exclude.Contains((ItemEnum)randomValue))
             {
                 randomValue = Random.Range(min, max + 1);
             }
+
             return randomValue;
         }
 
+        public float radius = 0.5f;
+        private bool _dragging = false;
 
-        private void OnTriggerEnter2D(Collider2D other)
+        private void Update()
         {
-            if(other.gameObject.name == "SoapCollider")
+            if (_dragging)
             {
-
-                initPos = transform.position;
-                TargetFish = other;
-            }
-
-  
-
-        }
-
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            if (collision.gameObject.name == "SoapCollider" )
-            {
-                TargetFish = null;
-                isCleaningFish = false;
-                cuoCount = 0;
-                maxInPath = transform.position;
-                initPos = transform.position;
-            }
-        }
-
-        private void OnTriggerStay2D(Collider2D collision)
-        {
-            if (collision.gameObject.name != "SoapCollider" || TargetFish != collision)
-            {
-                return;
-            }
-            // 第二次检测到，且没变targetfish，就比较位置，变化够大就完成一次搓
-
-
-            maxInPath.x = MathF.Max(maxInPath.x, transform.position.x);
-            maxInPath.y = MathF.Max(maxInPath.y, transform.position.y);
-            minInPath.x = MathF.Min(minInPath.x, transform.position.x);
-            minInPath.y = MathF.Min(minInPath.y, transform.position.y);
-            if (maxInPath.x - minInPath.x > 0.4f || maxInPath.y - minInPath.y > 0.4f)
-            {
-
-  
-                initPos = transform.position;
-                maxInPath = initPos;
-                minInPath = initPos;
-                cuoCount++;
-                if (cuoCount > 5)
+                var collider2Ds = Physics2D.OverlapCircleAll(transform.position, radius);
+                bool hit = false;
+                foreach (var collider2D1 in collider2Ds)
                 {
-                    // todo fsx
-                    var fish = collision.transform.parent.GetComponent<Fish>();
-                    var lastneedList = new List<ItemEnum>();
-                    lastneedList.AddRange(fish.needList);
-                        fish.needList.Remove(lastneedList[0]);
-                    var newitem = (ItemEnum)GetRandomExcluding((int)ItemEnum.蟹黄堡, (int)ItemEnum.核弹, lastneedList);
-                        fish.needList.Add(newitem);
+                    if (collider2D1.gameObject.name == ColliderName && _prevTarget == null)
+                    {
+                        _prevTarget = collider2D1;
+                        _prevHitFishPos = collider2D1.transform.position;
+                        hit = true;
+                        break;
+                    }
+
+                    if (collider2D1.gameObject.name == ColliderName && _prevTarget != null)
+                    {
+                        if (collider2D1 != _prevTarget)
+                        {
+                            _prevTarget = collider2D1;
+                            _prevHitFishPos = collider2D1.transform.position;
+                            hit = true;
+                            break;
+                        }
+
+                        hit = true;
+                        // 相同表示还是同一只鱼
+                        maxInPath.x = MathF.Max(maxInPath.x, transform.position.x);
+                        maxInPath.y = MathF.Max(maxInPath.y, transform.position.y);
+                        minInPath.x = MathF.Min(minInPath.x, transform.position.x);
+                        minInPath.y = MathF.Min(minInPath.y, transform.position.y);
+                        if (!(maxInPath.x - minInPath.x > 0.4f) && !(maxInPath.y - minInPath.y > 0.4f)) continue;
+                        _prevHitFishPos = transform.position;
+                        maxInPath = _prevHitFishPos;
+                        minInPath = _prevHitFishPos;
+                        cuoCount++;
+                        if (cuoCount > 5)
+                        {
+                            var fish = _prevTarget.transform.parent.GetComponent<Fish>();
+                            var lastneedList = new List<ItemEnum>();
+                            lastneedList.AddRange(fish.needList);
+                            fish.needList.Remove(lastneedList[0]);
+                            var newitem =
+                                (ItemEnum)GetRandomExcluding((int)ItemEnum.蟹黄堡, (int)ItemEnum.核弹, lastneedList);
+                            fish.needList.Add(newitem);
+                            cuoCount = 0;
+                        }
+                    }
+                }
+
+                if (!hit)
+                {
+                    _prevTarget = null;
                     cuoCount = 0;
+                    maxInPath = transform.position;
+                    _prevHitFishPos = transform.position;
                 }
             }
+        }
+
+        // private void OnTriggerEnter2D(Collider2D other)
+        // {
+        //     if (other.gameObject.name == "SoapCollider")
+        //     {
+        //         initPos = transform.position;
+        //         TargetFish = other;
+        //     }
+        // }
+
+        // private void OnTriggerExit2D(Collider2D collision)
+        // {
+        //     if (collision.gameObject.name == "SoapCollider")
+        //     {
+        //         TargetFish = null;
+        //         isCleaningFish = false;
+        //         cuoCount = 0;
+        //         maxInPath = transform.position;
+        //         initPos = transform.position;
+        //     }
+        // }
+
+        // private void OnTriggerStay2D(Collider2D collision)
+        // {
+        //     if (collision.gameObject.name != "SoapCollider" || fish_soap_collider != collision)
+        //     {
+        //         return;
+        //     }
+        //     // 第二次检测到，且没变targetfish，就比较位置，变化够大就完成一次搓
+        //
+        //
+        //     maxInPath.x = MathF.Max(maxInPath.x, transform.position.x);
+        //     maxInPath.y = MathF.Max(maxInPath.y, transform.position.y);
+        //     minInPath.x = MathF.Min(minInPath.x, transform.position.x);
+        //     minInPath.y = MathF.Min(minInPath.y, transform.position.y);
+        //     if (maxInPath.x - minInPath.x > 0.4f || maxInPath.y - minInPath.y > 0.4f)
+        //     {
+        //         initPos = transform.position;
+        //         maxInPath = initPos;
+        //         minInPath = initPos;
+        //         cuoCount++;
+        //         if (cuoCount > 5)
+        //         {
+        //             // todo fsx
+        //             var fish = collision.transform.parent.GetComponent<Fish>();
+        //             var lastneedList = new List<ItemEnum>();
+        //             lastneedList.AddRange(fish.needList);
+        //             fish.needList.Remove(lastneedList[0]);
+        //             var newitem = (ItemEnum)GetRandomExcluding((int)ItemEnum.蟹黄堡, (int)ItemEnum.核弹, lastneedList);
+        //             fish.needList.Add(newitem);
+        //             cuoCount = 0;
+        //         }
+        //     }
+        // }
+
+        private void OnMouseDown()
+        {
+            _dragging = true;
+        }
+
+        private void OnMouseUp()
+        {
+            _dragging = false;
         }
 
         public void OnMouseDrag()
@@ -114,9 +176,6 @@ namespace WitchFish
             objectPos.z = 0;
             //给物体赋值坐标
             transform.position = objectPos;
-
-
         }
     }
-
 }
